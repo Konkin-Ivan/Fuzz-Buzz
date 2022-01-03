26e0bd0299b06cb1288d7e2b1b97c565d8065fb7
Ivan <konkin.ivan@yandex.ru>
2022-01-04 10:29:38 +1200

    Modify js, css
    Добавил форматирование при выводе на страницу.

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 function number() {
   let min = 1 // минимальное значение
   let max = 100 // максимальное значение
-  const num = []
+  
+  const FuzzBuzz = document.querySelector('.fuzzbuzz-wrapper') // взять элемент из HTML
+  
+  /**
+   * Обработчик обертывания в тег span на каждой итерации, для возможности стилизации в css.
+   * @param {*} valueData 
+   * @param {*} styleSelector 
+   */
+  function pushValue(valueData, styleSelector) {
+    const num = document.createElement('span')
+    num.setAttribute('class', styleSelector)
+    num.innerHTML = valueData
+    FuzzBuzz.appendChild(num)
+  }
+
   for (min; min<max+1; min++) { // генератор чисел
     if (min % 15 === 0) { // проверка на кратность к 3 и 5
-      num.push('FuzzBuzz')
+      pushValue('FuzzBuzz', 'fuzzbuzz')
     } else if (min % 5 === 0) { // проверка на кратность к 5
-      num.push('Buzz')
+      pushValue('Buzz', 'buzz')
     } else if (min % 3 === 0) { // проверка на кратность к 3
-      num.push('Fuzz')
+      pushValue('Fuzz', 'fuzz')
     } else {
-      num.push(min) // запись результата в массив
+      pushValue(min, 'span') // запись результата в массив
     }
   }
-  formatArray = num.join(", ") // форматирование массива, для HTML
-  const FuzzBuzz = document.querySelector('.fuzzbuzz p') // взять элемент из HTML
-  FuzzBuzz.innerHTML = formatArray // вывести числа на страницу
-  console.log(num)
+  
 }
+
+
+
